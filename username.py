91f89f1ca70b256a3400139582f017486aac1e83
Roul_ <doyun.shin@gmail.com>
2019-11-21 00:25:57 +0900

    Update
    use config,json

--- username.py
+++ username.py
@@ -7,8 +7,6 @@ from parse import compile
 
 def main(data):
     while True:
-        dt = ''
-        tm = ''
         gdata = ''
         commandline = ''
         res = ''
@@ -26,36 +24,47 @@ def main(data):
         if gdata["success"] == True:
             print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+data["username"]+' is streaming.')
             # parsing
-            if "1080p60" in gdata["urls"]:    
-                m3u8id = gdata["urls"]["1080p60"]
-                quality = '1080p60'
-            elif "1080p" in gdata["urls"]:
-                m3u8id = gdata["urls"]["1080p"]
-                quality = '1080p'
-            elif "720p60" in gdata["urls"]:
-                m3u8id = gdata["urls"]["720p60"]
-                quality = '720p60'
-            elif "720p" in gdata["urls"]:
-                m3u8id = gdata["urls"]["720p"]
-                quality = '720p'
-            elif "480p" in gdata["urls"]:
-                m3u8id = gdata["urls"]["480p"]
-                quality = '480p'
-            elif "360p" in gdata["urls"]:
-                m3u8id = gdata["urls"]["480p"]
-                quality = '480p'
-            elif "160p" in gdata["urls"]:
-                m3u8id = gdata["urls"]["160p"]
-                quality = '160p'
+            if data['quality_enable'] == 1:
+                if data['quality'] in gdata['urls']:
+                    print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'Using config.json quality: '+data['quality'])
+                    m3u8id = gdata["urls"][data['quality']]
+                    quality = data['quality']
+                else:
+                    print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'ERROR: '+'Unknown quality. Program exit')
+                    exit()
+            else:
+                if "1080p60" in gdata["urls"]:    
+                    m3u8id = gdata["urls"]["1080p60"]
+                    quality = '1080p60'
+                elif "1080p" in gdata["urls"]:
+                    m3u8id = gdata["urls"]["1080p"]
+                    quality = '1080p'
+                elif "720p60" in gdata["urls"]:
+                    m3u8id = gdata["urls"]["720p60"]
+                    quality = '720p60'
+                elif "720p" in gdata["urls"]:
+                    m3u8id = gdata["urls"]["720p"]
+                    quality = '720p'
+                elif "480p" in gdata["urls"]:
+                    m3u8id = gdata["urls"]["480p"]
+                    quality = '480p'
+                elif "360p" in gdata["urls"]:
+                    m3u8id = gdata["urls"]["480p"]
+                    quality = '480p'
+                elif "160p" in gdata["urls"]:
+                    m3u8id = gdata["urls"]["160p"]
+                    quality = '160p'
+            
             # parsing end
             print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'Setting the quality to '+quality)
-            commandline = "streamlink -o '"+data["username"]+"-"+time.strftime('%Y-%m-%d.%H:%M:%S', time.localtime(time.time()))+".mp4' "+m3u8id+" best"
+            fhname = data["username"]+"-"+time.strftime('%Y-%m-%d.%Hh%Mm%Ss', time.localtime(time.time()))
+            commandline = "streamlink -o '"+fhname+".mp4' "+m3u8id+" best"
             os.system(commandline)
-            print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'moving the file named "'+data["username"]+"-"+time.strftime('%Y-%m-%d.%H:%M:%S', time.localtime(time.time()))+'".mp4')
-            commandline = "mv "+data["username"]+"-"+time.strftime('%Y-%m-%d.%H:%M:%S', time.localtime(time.time()))+".mp4 ../record/"+data["username"]+"/"+data["username"]+"-"+time.strftime('%Y-%m-%d.%H:%M:%S', time.localtime(time.time()))+".mp4"
+            print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'moving the file named "'+fhname+'".mp4')
+            commandline = "mv "+fhname+".mp4 "+data["mvtarget"]+"/"+fhname+".mp4"
             os.system(commandline)
         else: 
-            print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'WARN: '+data["username"]+'is not streaming')
+            print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'WARN: '+data["username"]+' is not streaming')
 
         print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'sleep', data["sleeptime"])
         time.sleep(data["sleeptime"])
@@ -68,14 +77,34 @@ if __name__ == "__main__":
     p = compile("{}.py")
     result = p.parse(os.path.basename(__file__))
     username = result[0]
-    
+    print('Starting the program named with '+username+'.py')
     # fix this
-
-    #username = 'streamername'
+    # if you use config.json, change True
+    IsConfigFileEnabled = True
+    # If IsConfigFileEnabled = True, You dont have to change this
     sleeptime = 180
+    quality_enable = 0
+    quality = ''
+    mvtarget = "../record/"+username
+
+    # Do not fix this
+    data = {"m3u8get": m3u8get, "username": username, "sleeptime": sleeptime, "quality_enable": quality_enable, "quality": quality, "mvtarget": mvtarget}
+
+    if IsConfigFileEnabled == True:
+        print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'Reading Config File')
+        fl = open('config.json','r')
+        f = json.load(fl)
+        data['sleeptime'] = f['sleeptime']
+        if f['username_enable'] == 1:
+            data['username'] = f['username']
+        if f['quality_enable'] == 1:
+            data['quality_enable'] = f['quality_enable']
+            data['quality'] = f['quality']
+        if f["mvtarget_enable"] == 1:
+            data['mvtarget'] = f['mvtarget']
+
+            
 
     # done
-    print('Starting the program')
-    print('Set streamer as '+username)
-    data = {"m3u8get": m3u8get, "username": username, "sleeptime": sleeptime}
+    print(time.strftime('[%Y-%m-%d | %H:%M:%S] ', time.localtime(time.time()))+'INFO: '+'Set streamer as '+data['username'])
     main(data)
